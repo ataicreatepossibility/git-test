b3a8179196815d2fddb7cc66d6f3c7c4710bea4c
zqt <208567240@qq.com>
2023-03-26 17:13:53 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,7 +4,8 @@ package com.atguigu.git;
  *
  */
 public class GitTest {
-    public static void main(System[] args){
+    public static void main(String[] args){
+        System.out.println("hello git");
         System.out.println("hello git");
     }
 }
